ba9869fc1a0d1925fcc6614ae9ee158dd8d05fd0
Medi Assumani <mediassumani49@gmail.com>
2017-09-23 20:33:57 -0400

    Update standardDeviation.cpp
    **Added comments

--- standardDeviation.cpp
+++ standardDeviation.cpp
@@ -1,3 +1,16 @@
+/**
+Coded By : Medi W. Assumani and Ripunjay Sigh
+
+Cs-180 Intro To Programming
+
+OBjective : Create a to find the average grade of the class ,the highest grade and the standard deviation of the grades 
+
+in a given array.
+
+
+
+*/
+
 #include <iostream>
 
 using namespace std;
@@ -5,38 +18,45 @@ using namespace std;
 int main()
 {
 //finding the average
-      int grades[]={89,95,72,83,99,54,86,75,92,73,79,75,83,73};
-      int sum=0;
-      int highest = grades[0];
-           int i;
-               for(i=0;i<14;i++)
+      int grades[]={89,95,72,83,99,54,86,75,92,73,79,75,83,73}; // this list(array) stores the gardes of a certain quiz
+      
+      int sum = 0;
+      
+      int highest = grades[0]; // getting the first grade to start comparing with other gardes
+          
+               for(int i = 0 ; i < 14 ; i++)// array size is 14
 
-                {sum += grades[i];
+                {
+                     sum + = grades[i]; // this adds up all the grades  
                 }
 
-                 int avg=sum/14;
+                 int avg = (sum/14); // finds the average grade of the class
 
-               cout<<"\nThe average is \n"<<avg<<endl;
+               cout<<"\nThe average is \n"<<avg<<endl; // prints the result
 
        //finding the highest grade
 
     for(int i = 1; i < 14; i++){
 
-      if(grades[i] >  highest){
+      if(grades[i] >  highest){ // comparing eah grade with another to find the highest
 
-     highest = grades[i];
+                  highest = grades[i]; //stores the current highest grade
 
      }
 
    }
 
-   cout <<"\nThe Highest grade is : \n"<<highest<<endl;
+   cout <<"\nThe Highest grade is : \n"<<highest<<endl; // printing the highest grade
 
-     int deviation[15];
+     int deviation[15]; // creates an array of 15 elements to store the standard deviation
+      
 cout<<"grades"<<"\t\t"<<"deviation"<<endl;
-for(i=0;i<14;i++)
-{deviation[i]=grades[i]-avg;
-cout<<grades[i] <<"\t\t"<<deviation[i]<<endl;
+      
+      for(int i = 0; i < 14; i++)
+{
+      deviation[i]=grades[i]-avg;// finding each deviation
+            
+      cout<<grades[i] <<"\t\t"<<deviation[i]<<endl; // printing the statndard deviation
 
 
 }
